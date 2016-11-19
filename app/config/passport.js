a8ef088319e3d3fa4510bfeff5642d4d5600b5da
Drew Fleeman <drew.fleeman@gmail.com>
2016-11-19 15:52:53 -0600

    add onfocusout validation for signup inputs

--- app/config/passport.js
+++ app/config/passport.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function (passport) {
-	
+
 	// =========================================================================
     // passport session setup ==================================================
     // =========================================================================
@@ -23,13 +23,13 @@ module.exports = function (passport) {
             done(err, user);
         });
     });
-    
+
     // =========================================================================
     // LOCAL SIGNUP ============================================================
     // =========================================================================
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
-	
+
 	passport.use('local-signup', new LocalStrategy({
         usernameField : 'username',
         passwordField : 'password',
@@ -50,8 +50,8 @@ module.exports = function (passport) {
             // check to see if theres already a user with that username
             if (user) {
                 return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-            } else if (password.length < 6) {
-                return done(null, false, req.flash('signupMessage', 'Password must be 6 or more characters.'));
+            } else if (password.length < 8) {
+                return done(null, false, req.flash('signupMessage', 'Password must be 8 or more characters.'));
             } else {
 
                 // if there is no user with that username
@@ -70,17 +70,17 @@ module.exports = function (passport) {
                 });
             }
 
-        });    
+        });
         });
 	  }
 	));
-        
+
         // =========================================================================
 	    // LOCAL LOGIN =============================================================
 	    // =========================================================================
 	    // we are using named strategies since we have one for login and one for signup
 	    // by default, if there was no name, it would just be called 'local'
-	
+
 	    passport.use('local-login', new LocalStrategy({
 	        // by default, local strategy uses username and password
 	        usernameField : 'username',
@@ -88,25 +88,25 @@ module.exports = function (passport) {
 	        passReqToCallback : true // allows us to pass back the entire request to the callback
 	    },
 	    function(req, username, password, done) { // callback with username and password from our form
-	
+
 	        // find a user whose username is the same as the form's username
 	        // we are checking to see if the user trying to login already exists
 	        User.findOne({ 'local.username' :  username }, function(err, user) {
 	            // if there are any errors, return the error before anything else
 	            if (err)
 	                return done(err);
-	
+
 	            // if no user is found, return the message
 	            if (!user)
 	                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-	
+
 	            // if the user is found but the password is wrong
 	            if (!user.validPassword(password))
 	                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-	
+
 	            // all is well, return successful user
 	            return done(null, user);
 	        });
-	
+
 	    }));
 };
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,139 @@
-$(document).ready(function() {
-      $('form').on('submit', function(e){
-          if ($('input[name="password"]').val() !== $('#verify-password').val()) {
-              e.preventDefault();
-              $('#verify').show();
-          }
-      });
+// signup form pre-submit validation
+var nameTooltip = $('#username-tooltip');
+
+var passwordOneTooltip = $('#password-1-tooltip');
+var passwordTwoTooltip = $('#password-2-tooltip');
+
+$('#username').focusout(function() {
+  if (!isNameValid()) {
+    nameTooltip.css('opacity', 1);
+  }
+});
+
+$('#username').focusin(function() {
+  nameTooltip.css('opacity', 0);
+});
+
+function isNameValid() {
+  var name = $('#username').val();
+  return name === "" ? false : true;
+}
+
+var firstPasswordInput = $('#password-input');
+var secondPasswordInput = $('#password-input-2');
+
+firstPasswordInput.focusout(function() {
+  var issues = passwordIssues();
+  if (!isPasswordValid(issues)) {
+    passwordOneTooltip.html('<span class="dot">!</span>' + issues);
+    passwordOneTooltip.css('opacity', 1);
+  }
+});
+
+firstPasswordInput.focusin(function() {
+  passwordOneTooltip.css('opacity', 0);
+});
+
+secondPasswordInput.focusout(function() {
+  var issues = secondPasswordIssues();
+  if (!isPasswordValid(issues)) {
+    passwordTwoTooltip.html('<span class="dot">!</span>' + issues);
+    passwordTwoTooltip.css('opacity', 1);
+  }
+});
+
+secondPasswordInput.focusin(function() {
+  passwordTwoTooltip.css('opacity', 0);
+});
+
+function passwordIssues() {
+  var firstPassword = firstPasswordInput.val();
+  var firstInputIssuesTracker = new IssueTracker();
+
+  if (firstPassword.length < 8) {
+    firstInputIssuesTracker.add("fewer than 8 characters");
+  } else if (firstPassword.length > 50) {
+    firstInputIssuesTracker.add("greater than 50 characters");
+  }
+
+  if (!firstPassword.match(/\d/g)) {
+    firstInputIssuesTracker.add("missing a number");
+  }
+
+  if (!firstPassword.match(/[a-z]/g)) {
+    firstInputIssuesTracker.add("missing a lowercase letter");
+  }
+
+  if (!firstPassword.match(/[A-Z]/g)) {
+    firstInputIssuesTracker.add("missing an uppercase letter");
+  }
+
+  return firstInputIssuesTracker.retrieve();
+}
+
+function secondPasswordIssues() {
+  var firstPassword = firstPasswordInput.val();
+  var secondPassword = secondPasswordInput.val();
+  var secondInputIssuesTracker = new IssueTracker();
+
+  if (firstPassword !== secondPassword) {
+    secondInputIssuesTracker.add("Passwords must match!");
+  }
+
+  return secondInputIssuesTracker.retrieve();
+}
+
+function isPasswordValid(issues) {
+  return issues.length > 0 ? false : true;
+}
+
+function IssueTracker() {
+  this.issues = [];
+}
+IssueTracker.prototype = {
+  add: function (issue) {
+    this.issues.push(issue);
+  },
+  retrieve: function () {
+    var message = "";
+    switch (this.issues.length) {
+      case 0:
+        // do nothing because message is already ""
+        break;
+      case 1:
+        message = "Please correct the following issue:\n" + this.issues[0];
+        break;
+      default:
+        message = "Please correct the following issues:\n" + this.issues.join(", ");
+        break;
+    }
+    return message;
+  }
+};
+
+$('#signup-submit').click(function(e) {
+  var preventDefault = false;
+
+  if (!isNameValid()) {
+    preventDefault = true;
+    nameTooltip.css('opacity', 1);
+  }
+
+  var issues = passwordIssues();
+  if (!isPasswordValid(issues)) {
+    preventDefault = true;
+    passwordOneTooltip.html('<span class="dot">!</span>' + issues);
+    passwordOneTooltip.css('opacity', 1);
+  }
+
+  var verificationIssues = secondPasswordIssues();
+  if (!isPasswordValid(verificationIssues)) {
+    preventDefault = true;
+    passwordTwoTooltip.html('<span class="dot">!</span>' + verificationIssues);
+    passwordTwoTooltip.css('opacity', 1);
+  }
+
+  if (preventDefault) {
+    e.preventDefault();
+  }
 });
